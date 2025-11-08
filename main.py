68903eec8b9d23449d88df6437bdad388199c0e8
Danru-RDRGZ99 <144158038+Danru-RDRGZ99@users.noreply.github.com>
2025-11-07 19:47:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -289,7 +289,7 @@ def main(page: ft.Page):
             nav_container = ft.Container(
                 content=nav_panel_content,
                 width=NAV_WIDTH, 
-                bgcolor=ft.Colors.BACKGROUND, 
+                bgcolor=ft.colors.BACKGROUND, # <--- CORRECCIÓN: 'Colors' a 'colors'
                 animate=ft.animation.Animation(300, "easeOutCubic"), 
             )
 
